851789409ed05a43c28a818bb05f09da105acc8a
Maor <maorbz858@gmail.com>
2024-04-13 14:48:01 +0300

    Before merge with `idan-profile`

--- app/src/main/java/com/hit/playpal/profile/data/repositories/ProfileRepository.java
+++ app/src/main/java/com/hit/playpal/profile/data/repositories/ProfileRepository.java
@@ -1,7 +1,11 @@
 package com.hit.playpal.profile.data.repositories;
 
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.hit.playpal.entities.chats.o2o.OneToOneChatRoom;
+import com.hit.playpal.entities.relationships.OneToOneChatRelationship;
 import com.hit.playpal.profile.data.datasources.ProfileFirebaseFirestoreDataSource;
 import com.hit.playpal.profile.domain.repositories.IProfileRepository;
 
@@ -40,6 +44,27 @@ public class ProfileRepository implements IProfileRepository {
     @Override
     public Task<Void> sendFriendRequest(String iReceiverId, String iSenderUid, String iSenderDisplayName, String iSenderProfileImage) {
         return DB.sendFriendRequest( iReceiverId,  iSenderUid,  iSenderDisplayName,  iSenderProfileImage).continueWith(task -> null);
+    }
+
+
+    @Override
+    public Task<QuerySnapshot> tryToGetOneToOneChatRelationship(String iUid1, String iUid2) {
+        return DB.tryToGetOneToOneChatRelationship(iUid1, iUid2);
+    }
+
+    @Override
+    public Task<DocumentSnapshot> getTheExistingOneToOneChatRoom(String iChatRoomId) {
+        return DB.getTheExistingOneToOneChatRoom(iChatRoomId);
+    }
+
+    @Override
+    public Task<DocumentReference> createAndGetNewOneToOneChatRoom(OneToOneChatRoom iOneToOneChatRoom) {
+        return DB.createAndGetNewOneToOneChatRoom(iOneToOneChatRoom);
+    }
+
+    @Override
+    public Task<Void> createNewOneToOneChatRelationship(OneToOneChatRelationship iOneToOneChatRelationship) {
+        return DB.createNewOneToOneChatRelationship(iOneToOneChatRelationship);
     }
 
 }
